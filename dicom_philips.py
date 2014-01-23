2449903b1d95ca1849d007c57071a142a15885b6
jstout211 <jstout211@yahoo.com>
2014-01-23 17:22:21 -0600

    Fixed typo, Works without throwing arbitrary errors

--- dicom_philips.py
+++ dicom_philips.py
@@ -15,13 +15,12 @@ import nibabel as nib
 Check for multivol then perform loop
 DONE: If completion, move to Processed folder (may have been done)
 DONE: Remove 0:5 condition on main loop
-Assumption: Scale on all multivolume images is the same"""
+Assumption: Scale on all multivolume images is the same
+May want to output a text file of subjid and dicom_name completed + errors"""
 
 
-
-
-#%% Folder Get
 def get_folder():
+    """Retrieve Folder using QT file browser"""    
     try:
         dialog = QApplication([])
         dialog = QFileDialog()
@@ -59,17 +58,11 @@ def folder_process_dicoms():
             convert_dicom(dataset, temp_folder)
             correct_dicom(dataset, temp_folder, output_folder)
             flush_temp_folder(temp_folder, error_folder)
-            print "Dicom Converted Successfully:\t ", i
+            print "Dicom Converted and Corrected Successfully:\t ", i
         except:
             print "Error in Dicom Process!!: \t ", i
             move_to_error_folder(temp_folder, error_folder)
-     
-        
-#            move_dicom_fail()
-    #dataset = glob.fnmatch.filter(os.listdir(dataset_folder), '*.nii*')
-    #dat= [g for i in dataset: g = dataset_folder + i]
-    
-    
+ 
 def convert_dicom(dicom_file, output_folder):
     """Use mcverter (from MRIConvert) to convert Dicom >> Nifti"""    
     mcverter_input="mcverter -o {0} -f {1}   -n -u -q -F -PatientName,+PatientId,-SeriesDate,-SeriesTime,-StudyId,-StudyDescription,+SeriesNumber,-SequenceName,+ProtocolName,-SeriesDescription {2}".format(output_folder, 'fsl', dicom_file)      
@@ -115,169 +108,19 @@ def correct_dicom(dicom_file, temp_nifti_folder, processed_folder):
             os.remove(os.path.join(temp_nifti_folder,curr_nifti_file))
         except:
             return Exception
-    
-
-                
-        
+                       
 def flush_temp_folder(temp_folder, error_folder):
     if os.listdir(temp_folder) != []:
         for filename in os.listdir(temp_folder):
-            shutil.move(os.path.join(temo_folder, filename), error_folder)
-
-    
-    
+            shutil.move(os.path.join(temp_folder, filename), error_folder)
         
 def move_to_error_folder(temp_folder, error_folder):
-    temp_contents=test=[os.path.join(temp_folder,i) for i in os.listdir(temp_folder)]
+    temp_contents=[os.path.join(temp_folder,i) for i in os.listdir(temp_folder)]
     [shutil.move(file, error_folder) for file in temp_contents]
-
-
-
-folder_process_dicoms()
-    
-
-
-
-
-
-
-
-##%%  REMVOE ALL OF BELOW
-#from mvpa2.suite import *
-#import nibabel as nib
-#os.chdir('/home/jeff/Desktop/TempTempFile/TestFixed')
-#
-#
-#ds=dicom.read_file('IM_0005')
-#pix=ds.pixel_array
-#
-#img=nib.load('./Test.nii')
-#
-#hdr=img.get_header()
-#hdr.data_to_fileobj(pix,img)
-#pix2=pix.swapaxes(0,2)
-#
-#
-#
-#
-#
-#
-#
-#
-#
-##tempvar={}
-##for i,j in enumerate(inputs):
-##    try:    
-##        ds=dicom.read_file(inputs[i])
-##        pix=ds.pixel_array
-##        if pix.shape[0] > 10:
-##            tempvar[i]=inputs[i]            
-##            #tempvar.append(inputs[i])
-##    except:
-##        print "Cant process ", inputs[i]
-##        
-##for i,z in enumerate(inputs):
-##    print i
-##    
-#    
-#    
-##%%         Find TFE SENSE
-#tempvar={}
-#prot_name={}
-#for i,j in enumerate(inputs[0:100]):
-#    try:    
-#        ds=dicom.read_file(inputs[i])
-#        prot_name[i]=ds[0x0018,0x1030].value
-#        #pix=ds.pixel_array
-#        #if pix.shape[0] > 10:
-#        #    tempvar[i]=inputs[i]            
-#            #tempvar.append(inputs[i])
-#    except:
-#        print "Cant process ", inputs[i]
-#        
-#for i,z in enumerate(inputs):
-#    print i, prot_name[i]
-#      
-#    
-#    
-#    
-#    
-##rescale_intercept=ds[0x0028,0x1052]    
-##rescale_slope=ds[0x0028,0x1053] 
-#
-#output={}
-#maxoutput={}
-#for i,j in  enumerate(tempvar.keys()): #enumerate(tempvar):
-#    #print j
-#    ds=dicom.read_file(tempvar[j])
-#    try:
-#        slope=ds.PerframeFunctionalGroups[0][0x2005,0x140f][0][0x0028,0x1053].value
-#        intercept=ds.PerframeFunctionalGroups[0][0x2005,0x140f][0][0x0028,0x1052].value
-#        scale_type=ds.PerframeFunctionalGroups[0][0x2005,0x140f][0][0x0028,0x1054].value
-#        output[i]=[slope,intercept,scale_type]
-#    except:
-#        try:
-#            slope=ds[0x0028,0x1053].value
-#            intercept=ds[0x0028,0x1052].value
-#            scale_type=ds[0x0028,0x1054].value
-#            output[i]=[slope,intercept,scale_type]
-#        except:
-#            output[i]=["noval","noval"]            
-#    #dim=pix.shape[0]/2    
-#    #imshow(pix[dim,:,:]) 
-#    try: 
-#        pix=ds.pixel_array
-#        maxoutput[i]=pix.max()
-#    except:
-#        pass
-#    
-#for i in range(len(output)):
-#    if output[i][0] != 'noval':
-#        #print output[i], maxoutput[i]
-#        print i, output[i][0], maxoutput[i]/output[i][0]
-#    
-#
-#ds=dicom.read_file(inputs[4])
-#
-#
-##%%    
-#imshow(pix[45,:,:]) 
-#np.max(np.max(np.max(pix))) 
-#
-#
-#slope=ds.PerframeFunctionalGroups[0][0x2005,0x140f][0][0x0028,0x1053].value
-#intercept=ds.PerframeFunctionalGroups[0][0x2005,0x140f][0][0x0028,0x1052].value
-#
-##%%
-#import dicom
-#from mvpa2.suite import *
-#import nibabel as nib
-#os.chdir('/home/jeff/Desktop/TempTempFile/TestFixed')
-#
-#
-#ds=dicom.read_file('IM_0005')
-#pix=ds.pixel_array
-#
-#img=nib.load('./Test.nii')
-#
-#hdr=img.get_header()
-#hdr.data_to_fileobj(pix,img)
-#pix2=pix.swapaxes(0,2)
-
-#%%
-   
+  
 if __name__ == "__main__":
     """Runs the main loop for import.  If prompt=True, then user inputs will be given, otherwise it will autoload TBSS_Data3"""
     #output_dataset = main(prompt = True)
     output_dataset = folder_process_dicoms()
-
-
-
-
-
-
-
-
-
 
 
